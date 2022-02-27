19fec00828160d43c8457c64f9621e92a5d18110
Vitaly Reutsky <vreutsky77@mail.ru>
2022-02-27 21:15:18 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -203,6 +203,10 @@ var rules1 = [{
 var rules2 = [{
   ruleSelector: ".form__input-name",
   rules: [{
+    rule: "customRegexp",
+    value: /^[A-zА-яЁё]+$/,
+    errorMessage: "Введите только буквы"
+  }, {
     rule: "minLength",
     value: 3,
     errorMessage: "Введите минимум 3 символов"
@@ -216,6 +220,10 @@ var rules2 = [{
   tel: true,
   telError: "Введите корректный телефон",
   rules: [{
+    rule: "customRegexp",
+    value: /[0-9]/,
+    errorMessage: "Введите только буквы"
+  }, {
     rule: "required",
     value: true,
     errorMessage: "Заполните телефон!"
