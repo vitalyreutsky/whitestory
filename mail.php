da192c4ccc4572d2a12e91aea62964bb329bf09e
Vitaly Reutsky <vreutsky77@mail.ru>
2022-03-02 12:03:17 +0300

    Updates

--- mail.php
+++ mail.php
@@ -9,16 +9,16 @@ $file = $_FILES['file'];
 
 $c = true;
 // Формирование самого письма
-$title = "Заголовок письма";
-foreach ( $_POST as $key => $value ) {
-  if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-    $body .= "
-    " . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+$title = "Тема письма";
+foreach ($_POST as $key => $value) {
+   if ($value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject") {
+      $body .= "
+    " . (($c = !$c) ? '<tr>' : '<tr style="background-color: #f8f8f8;">') . "
       <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
       <td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
     </tr>
     ";
-  }
+   }
 }
 
 $body = "<table style='width: 100%;'>$body</table>";
@@ -27,43 +27,28 @@ $body = "<table style='width: 100%;'>$body</table>";
 $mail = new PHPMailer\PHPMailer\PHPMailer();
 
 try {
-  $mail->isSMTP();
-  $mail->CharSet = "UTF-8";
-  $mail->SMTPAuth   = true;
+   $mail->isSMTP();
+   $mail->CharSet = "UTF-8";
+   $mail->SMTPAuth   = true;
 
-  // Настройки вашей почты
-  $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
-  $mail->Username   = ''; // Логин на почте
-  $mail->Password   = ''; // Пароль на почте
-  $mail->SMTPSecure = 'ssl';
-  $mail->Port       = 465;
+   // Настройки вашей почты
+   $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+   $mail->Username   = 'vreutsky77@gmail.com'; // Логин на почте
+   $mail->Password   = 'grquqdchcsxvecuh'; // Пароль на почте
+   $mail->SMTPSecure = 'ssl';
+   $mail->Port       = 465;
 
-  $mail->setFrom('', 'Заявка с вашего сайта'); // Адрес самой почты и имя отправителя
+   $mail->setFrom('vreutsky77@gmail.com', 'Заявка с вашего сайта'); // Адрес самой почты и имя отправителя
 
-  // Получатель письма
-  $mail->addAddress('');
+   // Получатель письма
+   $mail->addAddress('vreutsky77@mail.ru');
 
-  // Прикрипление файлов к письму
-  if (!empty($file['name'][0])) {
-    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
-      $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
-      $filename = $file['name'][$ct];
-      if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
-          $mail->addAttachment($uploadfile, $filename);
-          $rfile[] = "Файл $filename прикреплён";
-      } else {
-          $rfile[] = "Не удалось прикрепить файл $filename";
-      }
-    }
-  }
+   // Отправка сообщения
+   $mail->isHTML(true);
+   $mail->Subject = $title;
+   $mail->Body = $body;
 
-  // Отправка сообщения
-  $mail->isHTML(true);
-  $mail->Subject = $title;
-  $mail->Body = $body;
-
-  $mail->send();
-
+   $mail->send();
 } catch (Exception $e) {
-  $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+   $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
 }
