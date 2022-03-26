b41d36c3cfdc2d420105f3453764c5e6c1fdbed5
Vitaly Reutsky <vreutsky77@mail.ru>
2022-03-26 12:22:05 +0300

    added changed

--- src/js/components/scrollingHeader.js
+++ src/js/components/scrollingHeader.js
@@ -1,7 +1,21 @@
-window.addEventListener("scroll", () => {
-  let header = document.querySelector(".header__bottom");
-  let headerTop = document.querySelector(".header__top");
+let header = document.querySelector(".header__bottom");
+let headerTop = document.querySelector(".header__top");
+const body = document.body;
 
-//  headerTop.classList.toggle("hide", window.scrollY > 190);
-  header.classList.toggle("scrolling-active", window.scrollY > 190);
-});
+function funcEventListener() {
+  if (body.clientWidth >= 1024) {
+    if (window.scrollY > 190) {
+      headerTop.classList.add("hide-header");
+      header.classList.add("scrolling-active");
+    } else {
+      headerTop.classList.remove("hide-header");
+      header.classList.remove("scrolling-active");
+    }
+  } else {
+    headerTop.classList.remove("hide-header");
+    header.classList.remove("scrolling-active");
+  }
+}
+
+window.addEventListener("scroll", () => funcEventListener());
+window.addEventListener("resize", () => funcEventListener());
