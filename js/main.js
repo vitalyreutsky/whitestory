7c03d895fdd964d260e284c1e49dc84dc4cf2de2
Vitaly Reutsky <vreutsky77@mail.ru>
2022-04-07 22:11:03 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -419,31 +419,25 @@ function easeInOutCubic(t, b, c, d) {
   \**********************************************/
 /***/ (() => {
 
-var header = document.querySelector(".header__bottom");
-var headerTop = document.querySelector(".header__top");
-var body = document.body;
-
-function funcEventListener() {
-  if (body.clientWidth >= 1024) {
-    if (window.scrollY > 190) {
-      headerTop.classList.add("hide-header");
-      header.classList.add("scrolling-active");
-    } else {
-      headerTop.classList.remove("hide-header");
-      header.classList.remove("scrolling-active");
-    }
-  } else {
-    headerTop.classList.remove("hide-header");
-    header.classList.remove("scrolling-active");
-  }
-}
-
-window.addEventListener("scroll", function () {
-  return funcEventListener();
-});
-window.addEventListener("resize", function () {
-  return funcEventListener();
-});
+//let header = document.querySelector(".header__bottom");
+//let headerTop = document.querySelector(".header__top");
+//const body = document.body;
+//function funcEventListener() {
+//  if (body.clientWidth >= 1024) {
+//    if (window.scrollY > 190) {
+//      headerTop.classList.add("hide-header");
+//      header.classList.add("scrolling-active");
+//    } else {
+//      headerTop.classList.remove("hide-header");
+//      header.classList.remove("scrolling-active");
+//    }
+//  } else {
+//    headerTop.classList.remove("hide-header");
+//    header.classList.remove("scrolling-active");
+//  }
+//}
+//window.addEventListener("scroll", () => funcEventListener());
+//window.addEventListener("resize", () => funcEventListener());
 
 /***/ }),
 
