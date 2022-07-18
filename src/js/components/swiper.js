bad59f3175ceba10b125e244c9ea7a09cd478cf8
Vitaly Reutsky <vreutsky77@mail.ru>
2022-07-18 18:50:25 +0300

    Добавил фоновую анимацию на главную страницу сайта

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -1,6 +1,13 @@
 // Подключение свайпера
-import Swiper, { Navigation, Pagination, Autoplay, Keyboard } from "swiper";
-Swiper.use([Navigation, Pagination, Autoplay, Keyboard]);
+import Swiper, {
+  Navigation,
+  Pagination,
+  Autoplay,
+  Keyboard,
+  EffectFlip,
+  EffectFade,
+} from "swiper";
+Swiper.use([Navigation, Pagination, Autoplay, Keyboard, EffectFade]);
 const swiperAbout = new Swiper(".swiper-about", {
   autoplay: true,
   slidesPerView: 2.6,
@@ -73,4 +80,36 @@ const swiperStudies = new Swiper(".swiper-studies", {
       slidesPerView: 3,
     },
   },
+});
+
+const swiperTop = new Swiper(".top-swiper", {
+  autoplay: true,
+  slidesPerView: 1,
+  //  slidesPerGroup: 1,
+  spaceBetween: 0,
+  loop: true,
+  centeredSlides: true,
+  effect: "fade",
+  autoplay: {
+    delay: 2000,
+  },
+  speed: 1500,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  //  breakpoints: {
+  //    280: {
+  //      slidesPerView: 1,
+  //    },
+  //    320: {
+  //      slidesPerView: 1,
+  //    },
+  //    480: {
+  //      slidesPerView: 2,
+  //    },
+  //    992: {
+  //      slidesPerView: 3,
+  //    },
+  //  },
 });
